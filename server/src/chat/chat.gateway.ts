b8b706a87888c03bcc749abcde03dafe3e33ca02
isha-web1 <isha.web1@gmail.com>
2025-11-13 23:55:25 +0600

    complete and refactor

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -1,4 +1,5 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import type { Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
@@ -6,12 +7,41 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
   },
 })
 export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    console.log('Received message:', payload);
-    return 'Hello world!';
+  @SubscribeMessage('joinRoom')
+  async handleMessage(client: Socket, payload: any) {
+    console.log(`${payload} is joining the group.`);
+
+    await client.join('group');
+
+    // send to all
+    // io.to(ROOM).emit('roomNotice', userName);
+
+    // broadcast
+    client.to('group').emit('roomNotice', payload);
+    console.log('Received:', payload);
   }
-  handleConnection() {
-    console.log(`Client connected`);
+
+  @SubscribeMessage('chatMessage')
+  handleChatMessage(client: Socket, payload: any) {
+    client.to('group').emit('chatMessage', payload);
+  }
+
+  @SubscribeMessage('typing')
+  handleTyping(client: Socket, payload: any) {
+    client.to('group').emit('typing', payload);
+  }
+
+  @SubscribeMessage('stopTyping')
+  handleStopTyping(client: Socket, payload: any) {
+    client.to('group').emit('stopTyping', payload);
+  }
+
+  handleConnection(conn) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    console.log(`New client connected.`, conn.id);
+  }
+
+  handleDisconnect() {
+    console.log('Client has been disconnected');
   }
 }
